310e4293a7095ae1193ed1f44ddf73f2be6b3038
0xayman <aymantarig17@gmail.com>
2024-02-24 23:16:36 +0200

    feat: add property card

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,6 +17,29 @@ class PropertyType {
   });
 }
 
+class Property {
+  final double price;
+  final int bedrooms;
+  final int bathrooms;
+  final int sqauremeters;
+  final int halls;
+  final String imageUrl;
+  final String areaName;
+
+  Property({
+    required this.price,
+    required this.bedrooms,
+    required this.bathrooms,
+    required this.sqauremeters,
+    required this.halls,
+    required this.imageUrl,
+    this.areaName = "Giza, Egypt",
+  });
+}
+
+const String placeholderImage =
+    "https://th.bing.com/th/id/R.a1d4a6f8ba9cf40bbe69c6e47546e8a3?rik=dgUZMgnDeoL7Dw&riu=http%3a%2f%2fwww.luxxu.net%2fblog%2fwp-content%2fuploads%2f2017%2f02%2f20-Incredible-Modern-Houses-Around-the-United-States-5.jpg&ehk=jltOlopAEXlYw25Qjcb6BhHSadJcIyJ863PI4ffrO70%3d&risl=1&pid=ImgRaw&r=0";
+
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
@@ -37,6 +60,30 @@ class HomeScreen extends StatelessWidget {
       name: AppTranslations.duplex.tr,
       icon: Icons.home_work_rounded,
     ),
+  ];
+
+  final List<Property> properties = [
+    Property(
+        price: 65000,
+        bedrooms: 3,
+        bathrooms: 2,
+        sqauremeters: 155,
+        halls: 1,
+        imageUrl: placeholderImage),
+    Property(
+        price: 1700,
+        bedrooms: 2,
+        bathrooms: 2,
+        sqauremeters: 140,
+        halls: 1,
+        imageUrl: placeholderImage),
+    Property(
+        price: 17000,
+        bedrooms: 2,
+        bathrooms: 2,
+        sqauremeters: 145,
+        halls: 1,
+        imageUrl: placeholderImage),
   ];
 
   @override
@@ -83,22 +130,18 @@ class HomeScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  VerticalSpacer(height: 10.h),
                   Expanded(
                     child: ListView.separated(
                       padding: EdgeInsets.only(bottom: 20.h),
                       scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) => Container(
-                        height: 200.h,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.r),
-                          color: Colors.grey[200],
-                        ),
+                      itemBuilder: (context, index) => PropertyCard(
+                        property: properties[index],
                       ),
-                      separatorBuilder: (context, index) =>
-                          VerticalSpacer(height: 10.h),
-                      itemCount: 5,
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.blueGrey.shade300,
+                        height: 20.h,
+                      ),
+                      itemCount: properties.length,
                     ),
                   )
                 ],
@@ -108,6 +151,107 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: const BottomNavigation(),
+    );
+  }
+}
+
+class PropertyCard extends StatelessWidget {
+  const PropertyCard({
+    super.key,
+    required this.property,
+  });
+
+  final Property property;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.r),
+      ),
+      // padding: EdgeInsets.only(bottom: 10.w),
+      margin: EdgeInsets.only(top: 10.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 200.h,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10.r),
+              child: Image.network(
+                property.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+          ),
+          VerticalSpacer(height: 10.h),
+          Text(
+            "\$${property.price}",
+            style: TextStyle(
+              color: Colors.blueGrey.shade800,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          VerticalSpacer(height: 2.h),
+          Text(
+            property.areaName,
+            style: TextStyle(
+              color: Colors.blueGrey.shade800,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          VerticalSpacer(height: 5.h),
+          Row(
+            children: [
+              Icon(
+                Icons.king_bed_rounded,
+                color: Colors.blueGrey.shade400,
+                size: 20.sp,
+              ),
+              HorizontalSpacer(width: 5.w),
+              Text(
+                "${property.bedrooms}",
+                style: TextStyle(
+                  color: Colors.blueGrey.shade400,
+                  fontSize: 14.sp,
+                ),
+              ),
+              const Spacer(),
+              Icon(
+                Icons.bathtub_rounded,
+                color: Colors.blueGrey.shade400,
+                size: 20.sp,
+              ),
+              HorizontalSpacer(width: 5.w),
+              Text(
+                "${property.bathrooms} ",
+                style: TextStyle(
+                  color: Colors.blueGrey.shade400,
+                  fontSize: 14.sp,
+                ),
+              ),
+              const Spacer(),
+              Icon(
+                Icons.aspect_ratio_rounded,
+                color: Colors.blueGrey.shade400,
+                size: 20.sp,
+              ),
+              HorizontalSpacer(width: 5.w),
+              Text(
+                "${property.sqauremeters} mm",
+                style: TextStyle(
+                  color: Colors.blueGrey.shade400,
+                  fontSize: 14.sp,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
